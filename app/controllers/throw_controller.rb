d7183913019cc0d7cb113816a4853dc0f49cd334
Bode Fuwa <bodefuwa@gmail.com>
2015-04-22 08:31:45 -0400

    Added functionality

--- app/controllers/throw_controller.rb
+++ app/controllers/throw_controller.rb
@@ -5,15 +5,40 @@ class ThrowController < ApplicationController
   end
 
   def sign
-    @player   = params[:sign]
-    @computer = SIGNS.sample
-
-    if @player == @computer
-      flash[:success] = "Tied"
-      @result = "Tied"
-    else 
-    	flash[:success] = "You Lost"
-    	@result = "Lost"
-    end
+   @player   = params[:sign]
+   @computer = SIGNS.sample
+   play
   end
+
+  private
+
+    def play
+      case
+      when @computer == @player
+        @result = "tied"
+        flash_result(@result)
+      when @computer == "rock" && @player == "paper"   	
+      	@result = "lost"
+      	flash_result(@result)
+      when @computer == "paper" && @player == "rock"
+      	@result = "won"
+      	flash_result(@result)
+      	 when @computer == "rock" && @player == "scissors"    	 
+      	@result = "lost"
+      	flash_result(@result)
+      when @computer == "scissors" && @player == "rock"   	
+      	@result = "won"
+      	flash_result(@result)
+      when @computer == "paper" && @player == "scissors"     	
+      	@result = "lost"
+      	flash_result(@result)
+      when @computer == "scissors" && @player == "paper"
+      	@result = "won"
+      	flash_result(@result)
+      end
+    end
+
+    def flash_result(result)
+ 	  flash[:success] = "You " + result
+    end
 end
--- app/models/throw.rb
+++ app/models/throw.rb
@@ -2,24 +2,24 @@ class Throw < ActiveModel
    SIGNS = ['rock', 'paper', 'scissors']
 
    
-   def play(sign)
-     computer = SIGNS.sample
-     user 	  = sign
-     
-    case 
-      when computer == user
-        return "tie"
-      when computer == "rock" && user == "paper"
-      	return "loss"
-      when computer == "paper" && user == "rock"
-      	return "win"
-      	 when computer == "rock" && user == "scissors"
-      	return "loss"
-      when computer == "scissors" && user == "rock"
-      	return "win"
-      	 when computer == "paper" && user == "scissors"
-      	return "loss"
-      when computer == "sissors" && user == "paper"
-      	return "win"
-   end
+   def play(computer, player)
+      @computer = @computer
+      @player = player
+      case
+      when @computer == @player
+        @result = "tied"
+      when @computer == "rock" && @player == "paper"
+        @result = "lost"
+      when @computer == "paper" && @player == "rock"
+        @result = "won"
+         when @computer == "rock" && @player == "scissors"
+        @result = "lost"
+      when @computer == "scissors" && @player == "rock"
+        @result = "won"
+      when @computer == "paper" && @player == "scissors"
+        @result = "lost"
+      when @computer == "sissors" && @player == "paper"
+        @result = "won"
+      end
+    end
 end
