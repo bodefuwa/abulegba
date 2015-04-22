89e330e9c0d9d50825e81c4eb917f2ea93405626
Bode Fuwa <bodefuwa@gmail.com>
2015-04-22 08:08:21 -0400

    added game functionality

--- app/controllers/throw_controller.rb
+++ app/controllers/throw_controller.rb
@@ -1,8 +1,19 @@
 class ThrowController < ApplicationController
+    SIGNS = ['rock', 'paper', 'scissors']
+
   def index
   end
 
   def sign
-    throw.play(sign)
+    @player   = params[:sign]
+    @computer = SIGNS.sample
+
+    if @player == @computer
+      flash[:success] = "Tied"
+      @result = "Tied"
+    else 
+    	flash[:success] = "You Lost"
+    	@result = "Lost"
+    end
   end
 end
