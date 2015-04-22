4f40fd98a4ec2bcd5bef94cb6b27fc0b7b9514ff
Bode Fuwa <bodefuwa@gmail.com>
2015-04-22 08:41:07 -0400

    .

--- app/models/throw.rb
+++ app/models/throw.rb
@@ -1,25 +1,2 @@
 class Throw < ActiveModel
-   SIGNS = ['rock', 'paper', 'scissors']
-
-   
-   def play(computer, player)
-      @computer = @computer
-      @player = player
-      case
-      when @computer == @player
-        @result = "tied"
-      when @computer == "rock" && @player == "paper"
-        @result = "lost"
-      when @computer == "paper" && @player == "rock"
-        @result = "won"
-         when @computer == "rock" && @player == "scissors"
-        @result = "lost"
-      when @computer == "scissors" && @player == "rock"
-        @result = "won"
-      when @computer == "paper" && @player == "scissors"
-        @result = "lost"
-      when @computer == "sissors" && @player == "paper"
-        @result = "won"
-      end
-    end
 end
